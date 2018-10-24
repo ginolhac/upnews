769aeb5e532a69d677848b14f13fc2799ce866d7
Aurelien Ginolhac <ginolhac@gmail.com>
2018-10-24 15:00:13 +0200

    remove tibble dependency

--- R/check.R
+++ R/check.R
@@ -1,11 +1,8 @@
-#' @importFrom tibble tibble
-NULL
-
 #' fetch news from remote github repo
 #'
 #' no argument are used
 #'
-#' @return a tibble
+#' @return a data.frame
 #'
 #' @export
 upnews <- function() {
@@ -16,19 +13,22 @@ upnews <- function() {
   message(paste(length(outdated_repos), "outdated pkgs, fetching news..."))
   if (length(outdated_repos) == 0) {
     return(
-      tibble::tibble(
+      data.frame(
         pkgs = character(0),
         local = character(0),
         remote = character(0),
         news = character(0))
       )
   }
-  news <- lapply(repos[outdated_repos], fetch_news)
-  tibble(
+  news <- unlist(lapply(repos[outdated_repos], fetch_news))
+  df_news <- data.frame(
     pkgs = outdated_repos,
     local = local_version(local_gh_pkg()[outdated_repos]),
     remote = remote_version(repos[outdated_repos], remote_sha[outdated_repos]),
     news = news)
+  # trick to get tibble output without dependencies by Eric Koncina
+  class(df_news) <- c("tbl_df", "tbl", "data.frame")
+  df_news
 }
 
 
